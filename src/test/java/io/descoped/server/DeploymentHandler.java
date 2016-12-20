42e710f7b42aff7cc30916dc81d1a20da8b943e9
oranheim <oranheim@gmail.com>
2016-12-20 18:49:18 +0100

    Made deployment handler app scoped

--- src/test/java/io/descoped/server/DeploymentHandler.java
+++ src/test/java/io/descoped/server/DeploymentHandler.java
@@ -5,11 +5,13 @@ import io.descoped.server.container.UndertowContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 
 /**
  * Created by oranheim on 19/12/2016.
  */
+@ApplicationScoped
 public class DeploymentHandler {
 
     private static final Logger log = LoggerFactory.getLogger(DeploymentHandler.class);
