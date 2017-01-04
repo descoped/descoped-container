66b7746fb94139352d4c8b6b30cec5f4c6c7d70a
oranheim <oranheim@gmail.com>
2017-01-04 12:34:40 +0100

    added helper to support @HandleTypes

--- src/main/java/io/descoped/container/deployment/BaseDeployment.java
+++ src/main/java/io/descoped/container/deployment/BaseDeployment.java
@@ -6,10 +6,8 @@ import io.descoped.container.extension.ClassLoaders;
 import io.undertow.server.handlers.PathHandler;
 import io.undertow.server.handlers.resource.ClassPathResourceManager;
 import io.undertow.servlet.Servlets;
-import io.undertow.servlet.api.DeploymentInfo;
-import io.undertow.servlet.api.DeploymentManager;
-import io.undertow.servlet.api.FilterInfo;
-import io.undertow.servlet.api.ServletInfo;
+import io.undertow.servlet.api.*;
+import io.undertow.servlet.util.ImmediateInstanceFactory;
 import org.apache.deltaspike.cdise.servlet.CdiServletRequestListener;
 import org.apache.deltaspike.servlet.impl.event.EventBridgeContextListener;
 import org.apache.deltaspike.servlet.impl.event.EventBridgeFilter;
@@ -21,21 +19,37 @@ import org.glassfish.jersey.servlet.ServletContainer;
 import org.glassfish.jersey.servlet.ServletProperties;
 import org.jboss.weld.servlet.WeldInitialListener;
 import org.jboss.weld.servlet.WeldTerminalListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import javax.servlet.DispatcherType;
-import javax.servlet.Filter;
-import javax.servlet.Servlet;
-import javax.servlet.ServletException;
+import javax.servlet.*;
+import javax.servlet.annotation.HandlesTypes;
 import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 /**
  * Created by oranheim on 04/01/2017.
  */
 abstract public class BaseDeployment implements Deployment {
 
+    private static final Logger log = LoggerFactory.getLogger(BaseDeployment.class);
+
     abstract protected DeploymentManager getManager();
 
     abstract protected PathHandler getPath();
+
+    protected List<ServletContainerInitializerInfo> getServletContainerInitializers() {
+        List<ServletContainerInitializerInfo> infos = new ArrayList<>();
+        for (ServletContainerInitializer initializer : ServiceLoader.load(ServletContainerInitializer.class)) {
+            HandlesTypes types = initializer.getClass().getAnnotation(HandlesTypes.class);
+            log.trace("---> HandleTypes: {} ==> {}", initializer.getClass(), types);
+            infos.add(new ServletContainerInitializerInfo(
+                    initializer.getClass(),
+                    new ImmediateInstanceFactory<>(initializer),
+                    types == null ? Collections.emptySet() : new LinkedHashSet<>(Arrays.asList(types.value()))));
+        }
+        return infos;
+    }
 
     protected ServletInfo createServletInfo(String servletName, String mapping, Class<? extends Servlet> servlet,
                                             Class<? extends ResourceConfig> jaxRsResourceClass,
