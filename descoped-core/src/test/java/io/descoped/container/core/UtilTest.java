1fbcfcd9f58f3a18707013ac224880551c8852c1
oranheim <oranheim@gmail.com>
2017-02-02 07:56:51 +0100

    Enable UtilTest

--- descoped-core/src/test/java/io/descoped/container/core/UtilTest.java
+++ descoped-core/src/test/java/io/descoped/container/core/UtilTest.java
@@ -6,7 +6,6 @@ import org.apache.deltaspike.core.api.config.ConfigProperty;
 import org.apache.deltaspike.core.api.config.ConfigResolver;
 import org.apache.deltaspike.testcontrol.api.TestControl;
 import org.apache.deltaspike.testcontrol.api.junit.CdiTestRunner;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -18,7 +17,7 @@ import static org.junit.Assert.assertEquals;
 /**
  * Created by oranheim on 10/12/2016.
  */
-@Ignore
+//@Ignore
 @RunWith(CdiTestRunner.class)
 @TestControl(logHandler = ConsoleAppender.class)
 public class UtilTest {
